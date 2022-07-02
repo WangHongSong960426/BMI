289b7c25430ff7e38cccf7f0bfcb7ad47c775e3b
Wayne Wang <hsw960426@gmail.com>
2022-07-02 09:38:22 +0800

    202207020938

--- BMI.py
+++ BMI.py
@@ -2,8 +2,12 @@ kg = float (input ('your weight(KG):  '))
 m = float (input ('your height(M): '))
 print ('your weight: ', kg, 'your height: ', m)
 BMI = kg / (m*m)
-print (BMI)
-if BMI < 18.5: print ('過輕 請多吃點')
-if 18.5 <= BMI < 25.0: print ('標準 請維持身材')
-if 25.0 <= BMI < 30.0: print ('超重 請多做運動')
-if BMI >= 30.0: print ('肥胖 請少吃點')
+print ('your BMI: ', BMI)
+if BMI < 18.5: 
+	print ('過輕 請多吃點')
+if 18.5 <= BMI < 25.0: 
+	print ('標準 請維持身材')
+if 25.0 <= BMI < 30.0: 
+	print ('超重 請多做運動')
+if BMI >= 30.0: 
+	print ('肥胖 請少吃點')
